cb0ace0cf479eebf58a086ddd1fae5ddb69a249c
EliwtFdez <shadowkadizz@gmail.com>
2024-06-26 17:50:31 -0600

    Encrypt Password

--- controller/user.js
+++ controller/user.js
@@ -2,7 +2,6 @@
 
 const bcrypt = require('bcrypt-nodejs');
 const User = require('../models/user');
-const crypto = require('crypto');
 
 function pruebas(req, res) {
     res.status(200).send({
@@ -10,61 +9,41 @@ function pruebas(req, res) {
     });
 }
 
-function encryptData(data) {
-    const algorithm = 'aes-256-cbc';
-    const key = crypto.randomBytes(32);
-    const iv = crypto.randomBytes(16);
-    let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
-    let encrypted = cipher.update(data);
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex'), key: key.toString('hex') };
-}
-
 async function saveUser(req, res) {
-    const user = new User();
     const params = req.body;
-
-    console.log(params);
 
     if (!params.password || !params.name || !params.surname || !params.email) {
         return res.status(400).send({ message: 'Enter all required fields' });
     }
 
-    const encryptedName = encryptData(params.name);
-    const encryptedSurname = encryptData(params.surname);
-    const encryptedEmail = encryptData(params.email);
+    const user = new User({
+        name: params.name,
+        surname: params.surname,
+        email: params.email,
+        role: 'ROLE_USER',
+        image: 'null'
+    });
 
-    user.name = encryptedName.encryptedData;
-    user.surname = encryptedSurname.encryptedData;
-    user.email = encryptedEmail.encryptedData;
-    user.role = 'ROLE_ADMIN';
-    user.image = 'null';
+    bcrypt.hash(params.password, null, null, async function(err, hash) {
+        if (err) {
+            return res.status(500).send({ message: 'Error encrypting password' });
+        }
+        user.password = hash;
 
-    if (params.password) {
-        bcrypt.hash(params.password, null, null, async function(err, hash) {
-            if (err) {
-                return res.status(500).send({ message: 'Error encrypting password' });
+        try {
+            const userStored = await user.save();
+            if (!userStored) {
+                return res.status(404).send({ message: 'User not registered' });
             }
-            user.password = hash;
-
-            try {
-                const userStored = await user.save();
-                if (!userStored) {
-                    return res.status(404).send({ message: 'User not registered' });
-                }
-                res.status(200).send({ user: userStored });
-            } catch (error) {
-                res.status(500).send({ message: 'Error saving user' });
-            }
-        });
-    } else {
-        res.status(400).send({ message: 'Enter password' });
-    }
+            res.status(200).send({ user: userStored });
+        } catch (error) {
+            res.status(500).send({ message: 'Error saving user' });
+        }
+    });
 }
 
 async function loginUser(req, res) {
     const params = req.body;
-
     const email = params.email;
     const password = params.password;
 
