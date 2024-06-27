e7ad31c62f614bad873afc110d623c0017c173ba
EliwtFdez <shadowkadizz@gmail.com>
2024-06-26 19:27:21 -0600

    Jwt Services

--- services/jwt.js
+++ services/jwt.js
@@ -2,7 +2,7 @@
 
 var jwt = require('jwt-simple');
 var moment = require('moment');
-var secret = 'clave_top_Secret_UwU?'
+var secret = 'clave_top_Secret_UwU?';
 
 exports.createToken = function (user){
     var payload = {
